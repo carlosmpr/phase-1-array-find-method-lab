b9e3bbd03393a1b4a542d898e90c8037936b06cf
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-05-18 15:55:18 -0400

    commit tree 320b47b6083ebe3d3767721aef06a53a06de8c7d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+] 
+
+const superbowlWin = (array) => {
+    const result = array.find( element => element.result === 'W')
+    if(result){
+        return result.year
+    }
+}
